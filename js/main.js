1520252b692a64d73aaa266633dc715bd6399474
KyleA16 <ali.kylem@gmail.com>
2024-03-28 13:51:32 -0700

    Add code section headers

---
+++ js/main.js
@@ -0,0 +1,17 @@
+  /*----- constants -----*/
+
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+    //click to start the game
+
+    //click cli letter
+
+  /*----- functions -----*/
+
